02a59b0b6f727e518a6b26c3bf4a68ff06706e50
Adam Ashkenazi <adammashkenazi@gmail.com>
2019-02-14 06:07:54 -0500

    Adding in .gitingore and .gitattributes to repo. Minor updates to index.html and app.js

--- js/app.js
+++ js/app.js
@@ -16,16 +16,11 @@ if (numTiles % 2 !== 0) {
   numTiles = numTiles - 1;
 }
 
+// Create list of symbols in gameplay
+const symbolsInGame = symbols.slice(0,numTiles/2);
 
 
 
-// Functions to assist with board initilization
-function createCard(symbol) {
-  card = document.createElement('li');
-  card.className = "card";
-  card.innerHTML = `<i class="fa ${symbol}"></i>`;
-  return card;
-}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -42,18 +37,55 @@ function shuffle(array) {
     return array;
 }
 
+// Initialize Game and/or reset
 
-// Create list of symbols in gameplay
-const symbolsInGame = symbols.slice(0,numTiles/2);
-const randomizedSymbolList = shuffle([...symbolsInGame, ...symbolsInGame])
+function deleteAllCards() {
+  while (deck.children.length > 0) {
+    deck.firstElementChild.remove();
+  }
+}
+
+// Create HTML Element and associated detail for cards
+function createCardElement(symbol) {
+  card = document.createElement('li');
+  card.className = "card";
+  card.innerHTML = `<i class="fa ${symbol}"></i>`;
+  return card;
+}
 
 // Create HTMLElements associated with symbols and add them to document
-const fragment = document.createDocumentFragment();
-for (symbol of randomizedSymbolList) {
-    const newCard = createCard(symbol);
-    fragment.appendChild(newCard);
+function createCards(randomizedSymbolList) {
+  const fragment = document.createDocumentFragment();
+  for (symbol of randomizedSymbolList) {
+      const newCard = createCardElement(symbol);
+      fragment.appendChild(newCard);
+  }
+  deck.appendChild(fragment);
 }
-deck.appendChild(fragment);
+
+function resetStats() {
+  document.querySelector('.timer').innerText = 0.0;
+  document.querySelector('.moves').innerText = 0;
+  numMoves = 0;
+  startTime = undefined;
+  timeElapsed = undefined;
+  gameCompleted = false;
+}
+
+function reset(symbolsInGame) {
+  // Reset Game Details including time elapsed and number of moves
+  resetStats();
+  // If cards exists, delete existing tiles
+  if (deck.children.length > 0) {
+    deleteAllCards();
+  }
+  // Randomize symbols and add back in cards;
+  randomizedSymbolList = shuffle(symbolsInGame);
+  createCards(randomizedSymbolList);
+}
+
+// Initialize Game
+reset([...symbolsInGame, ...symbolsInGame]);
 
 
 
@@ -70,6 +102,8 @@ deck.appendChild(fragment);
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+
+//// Functions to faciliate gameplay including clicking on cards and matching criteria
 function displaySymbol(card) {
   card.className = "card show";
 };
@@ -103,14 +137,14 @@ function cardMatchCheck() {
       onMismatch();
       openList = [];
     }, 500);
-  }
-};
+  };
+}
 
 function incrementMoveCounter() {
   moves = document.querySelector('.moves');
   numMoves = numMoves + 1;
   moves.innerText = numMoves;
-};
+}
 
 function updateTimer() {
   if (startTime && !gameCompleted) {
@@ -119,6 +153,10 @@ function updateTimer() {
     setTimeout(updateTimer,50);
   }
 }
+
+
+//TODO add in modal box with game info and buttons to start new game;
+//TODO add in scoring for stars based on number of moves/time elapsed;  
 function gameCompleteCheck() {
   let cardList = document.querySelectorAll('.card');
   let gameCompleted = true;
@@ -126,8 +164,10 @@ function gameCompleteCheck() {
     gameCompleted = gameCompleted && card.classList.contains('match');
   }
   return gameCompleted;
-};
+}
 
+
+// Add event listener to react to click on card
 deck.addEventListener('click', function(event) {
   if (event.target.className === "card") {
     // Start Timer On First Tile Click
@@ -136,16 +176,32 @@ deck.addEventListener('click', function(event) {
       updateTimer()
     }
 
+    //Display tile after it's clicked and add it to list of currently opened cards
     displaySymbol(event.target);
     addToOpenList(event.target);
+    // If there is another open card, check if new card matches
     if (openList.length == 2) {
      cardMatchCheck();
     };
-
+    // Update check to see whether game is done;
     gameCompleted = gameCompleteCheck()
     if (gameCompleted) {
       console.log(`Woo! Game completed in ${timeElapsed} seconds`)// TODO Make gamecomplete hovering modal appear with details
     }
   }
-
 });
+
+// Add event listener to reset game
+restart = document.querySelector('.restart')
+restart.addEventListener('click', function() {
+    reset([...symbolsInGame, ...symbolsInGame]);
+  }
+);
+//// Debugging Functions
+// Function to automatically win the game
+function winGame() {
+  for (card of deck.children) {
+    card.className = "card show match";
+  }
+  gameCompleted = true;
+}
